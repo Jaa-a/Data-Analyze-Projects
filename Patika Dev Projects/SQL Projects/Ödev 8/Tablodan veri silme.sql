21fd1efc7bb8f540697405f8f2f95d360aaceff9
Jaa <emre.ilcim@gmail.com>
2023-06-16 17:21:23 +0300

    Create Tablodan veri silme.sql

---
+++ Patika Dev Projects/SQL Projects/Ödev 8/Tablodan veri silme.sql
@@ -0,0 +1,14 @@
+DELETE FROM employee
+WHERE id = 11;
+
+DELETE FROM employee
+WHERE id = 12;
+
+DELETE FROM employee
+WHERE id = 13;
+
+DELETE FROM employee
+WHERE id = 14;
+
+DELETE FROM employee
+WHERE id = 15;
